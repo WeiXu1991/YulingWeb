9b18becc4ca6ffa758370a63c3801f2a68302c89
Wei Xu <xwxuweinupt@gmail.com>
2019-08-04 14:36:44 -0700

    [master] added assets files and css files for home page

---
+++ public/js/home.js
@@ -0,0 +1,77 @@
+/* jshint esnext: true */
+/* jslint browser: true */
+/* global window document $ */
+$(document).ready(($) => {
+  const textChangeSpeed = 250;
+  let selfIntroYOffset = $('.self-intro').offset().top;
+  let navHeight = $('nav').outerHeight();
+
+  $('.navbar-nav').addClass('hide-nav-links');
+
+  $(window).scroll(() => {
+    if (window.pageYOffset + navHeight > selfIntroYOffset) {
+      $('.navbar-nav').removeClass('hide-nav-links');
+    } else {
+      $('.navbar-nav').addClass('hide-nav-links');
+    }
+  });
+
+  $(window).resize(() => {
+    selfIntroYOffset = $('.self-intro').offset().top;
+    navHeight = $('nav').outerHeight();
+  });
+
+  function appendLetter(letter) {
+    $('.txt-rotate .wrap').text($('.txt-rotate .wrap').text() + letter);
+  }
+
+  function removerLastLetter() {
+    $('.txt-rotate .wrap').text($('.txt-rotate .wrap').text().substring(0, $('.txt-rotate .wrap').text().length - 1));
+  }
+
+  function showNextLetter(textArray, index, letterIndex, mover) {
+    const textString = textArray[index];
+    if (letterIndex === 0 && mover === 1) {
+      appendLetter(textString[letterIndex]);
+      window.setTimeout(() => {
+        showNextLetter(textArray, index, letterIndex + 1, mover);
+      }, textChangeSpeed);
+    } else if (letterIndex === 0 && mover === -1) {
+      removerLastLetter();
+      if (index === textArray.length - 1) {
+        window.setTimeout(() => {
+          showNextLetter(textArray, 0, 0, 1);
+        }, 4 * textChangeSpeed);
+      } else {
+        window.setTimeout(() => {
+          showNextLetter(textArray, index + 1, 0, 1);
+        }, 4 * textChangeSpeed);
+      }
+    } else if (letterIndex === textArray[index].length - 1 && mover === 1) {
+      appendLetter(textString[letterIndex]);
+      window.setTimeout(() => {
+        showNextLetter(textArray, index, letterIndex, -1);
+      }, 4 * textChangeSpeed);
+    } else if (letterIndex === textArray[index].length - 1 && mover === -1) {
+      removerLastLetter();
+      window.setTimeout(() => {
+        showNextLetter(textArray, index, letterIndex - 1, -1);
+      }, textChangeSpeed / 2);
+    } else if (mover === 1) {
+      appendLetter(textString[letterIndex]);
+      window.setTimeout(() => {
+        showNextLetter(textArray, index, letterIndex + 1, mover);
+      }, textChangeSpeed);
+    } else if (mover === -1) {
+      removerLastLetter();
+      window.setTimeout(() => {
+        showNextLetter(textArray, index, letterIndex - 1, -1);
+      }, textChangeSpeed / 2);
+    }
+  }
+
+  const textArray = ['Yuling Pan', 'an UX/UI designer'];
+  if (textArray && textArray.length > 0) {
+    showNextLetter(textArray, 0, 0, 1);
+  }
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,51 @@
 /* jshint esnext: true */
-// eslint-disable-next-line no-undef
+/* jslint browser: true */
+/* global window document $ navigator */
 $(document).ready(($) => {
-  const textChangeSpeed = 250;
 
-  function appendLetter(letter) {
-    $('.txt-rotate .wrap').text($('.txt-rotate .wrap').text() + letter);
+
+  // Use 2x image for retina display by default
+  // change to 1x for low resolution screen.
+  if (window.devicePixelRatio && window.devicePixelRatio < 2) {
+    const allImages = $('img');
+    for (let i = 0; i < allImages.length; i += 1) {
+      allImages[i].src = allImages[i].src.replace('@2x', '');
+    }
   }
 
-  function removerLastLetter() {
-    $('.txt-rotate .wrap').text($('.txt-rotate .wrap').text().substring(0, $('.txt-rotate .wrap').text().length - 1));
-  }
+
 
   //   function showText(textArray, index) {
   //     const text = textArray[index];
   //   }
 
   // eslint-disable-next-line no-unused-vars
-  function showNextLetter(textArray, index, letterIndex, mover) {
-    const textString = textArray[index];
-    if (letterIndex === 0 && mover === 1) {
-      appendLetter(textString[letterIndex]);
-      window.setTimeout(() => { showNextLetter(textArray, index, letterIndex + 1, mover); }, textChangeSpeed);
-    } else if (letterIndex === 0 && mover === -1) {
-      removerLastLetter();
-      if (index === textArray.length - 1) {
-        window.setTimeout(() => { showNextLetter(textArray, 0, 0, 1); }, 4 * textChangeSpeed);
-      } else {
-        window.setTimeout(() => { showNextLetter(textArray, index + 1, 0, 1); }, 4 * textChangeSpeed);
-      }
-    } else if (letterIndex === textArray[index].length - 1 && mover === 1) {
-      appendLetter(textString[letterIndex]);
-      window.setTimeout(() => { showNextLetter(textArray, index, letterIndex, -1) ;}, 4 * textChangeSpeed);
-    } else if (letterIndex === textArray[index].length - 1 && mover === -1) {
-      removerLastLetter();
-      window.setTimeout(() => { showNextLetter(textArray, index, letterIndex - 1, -1); }, textChangeSpeed / 2);
-    } else if (mover === 1) {
-      appendLetter(textString[letterIndex]);
-      window.setTimeout(() => { showNextLetter(textArray, index, letterIndex + 1, mover); }, textChangeSpeed);
-    } else if (mover === -1) {
-      removerLastLetter();
-      window.setTimeout(() => { showNextLetter(textArray, index, letterIndex - 1, -1) ;}, textChangeSpeed / 2);
-    }
-  }
+
 
   $('#email-nav').click((e) => {
     e.preventDefault();
   });
   $('#copy-email').click((e) => {
     e.preventDefault();
-    navigator.permissions.query({name: "clipboard-write"}).then(result => {
-      if (result.state == "granted" || result.state == "prompt") {
-        navigator.clipboard.writeText("ypandesign@gmail.com").then(() => {
+    navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+      if (result.state === 'granted' || result.state === 'prompt') {
+        navigator.clipboard.writeText('ypandesign@gmail.com').then(() => {
           $('#email-copy-success').fadeIn(200).delay(2000).fadeOut(1000);
         }, (err) => {
-        // eslint-disable-next-line indent
-        console.log(err);
-          return;
+          // eslint-disable-next-line no-console
+          console.log(err);
         });
       }
     });
   });
   // let textArray = $('.txt-rotate').data('rotate');
   // console.log(textArray);
-  const textArray = ['Yuling Pan', 'an UX/UI designer'];
-  if (textArray && textArray.length > 0) {
-    // showNextLetter(textArray, 0, 0, 1);
-  }
+
+
+  // $(window).scroll(() => {
+  //   const offsetTop = window.pageYOffset;
+  //   if (offsetTop > 80) {
+  //     console.log(80);
+  //   }
+  // });
 });
