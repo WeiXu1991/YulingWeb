abed5bed4f65cded14e3dbee5828bb3b0cff0d91
Wei Xu <xwxuweinupt@gmail.com>
2019-10-13 14:03:22 -0700

    changed imgs

--- public/js/project.js
+++ public/js/project.js
@@ -1,6 +1,6 @@
 /* jshint esnext: true */
 /* jslint browser: true */
-/* global window document $ */
+/* global window document $ navigator */
 $(document).ready(($) => {
   const navHeight = $('nav').outerHeight();
   let navPositionTop = -parseInt($('.navbar.fixed-top').css('top'), 10);
@@ -30,5 +30,21 @@ $(document).ready(($) => {
       top: `${-navPositionTop}px`,
     });
     preYOffset = window.pageYOffset;
+  });
+  $('#nav-email-copy').click((e) => {
+    e.preventDefault();
+  });
+  $('#nav-email-copy').click((e) => {
+    e.preventDefault();
+    navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+      if (result.state === 'granted' || result.state === 'prompt') {
+        navigator.clipboard.writeText('ypandesign@gmail.com').then(() => {
+          $('#nav-email-copy-success').fadeIn(200).delay(2000).fadeOut(1000);
+        }, (err) => {
+          // eslint-disable-next-line no-console
+          console.log(err);
+        });
+      }
+    });
   });
 });
